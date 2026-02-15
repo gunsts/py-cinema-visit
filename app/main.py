52565c04652d945f34e87c483e33d1b618cbfb14
gunsts <137734013+gunsts@users.noreply.github.com>
2026-02-16 06:18:29 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from people.cinema_staff import Cleaner
 def cinema_visit(customers: list,
                  hall_number: int,
                  cleaner: str,
-                 movie: str):
+                 movie: str) -> None:
     list_of_customers = [Customer(customer["name"], customer["food"])
                          for customer in customers]
     for customer in list_of_customers:
